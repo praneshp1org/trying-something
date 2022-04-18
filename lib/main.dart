023c0921a0f4e1f334e32db403d688b7a481965c
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-18 11:34:28 +0545

    khhh

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        primarySwatch: Colors.blue,
       ),
       routes: {
         "/": (_) => HomePage(),
