affdeeea1ea1ad53ae3e865cb8aa19d17ccdc003
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-19 18:57:24 +0545

    retrying json

---
+++ lib/pages/json_011.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JSON011 extends StatefulWidget {
+  const JSON011({Key? key}) : super(key: key);
+
+  @override
+  State<JSON011> createState() => _JSON011State();
+}
+
+class _JSON011State extends State<JSON011> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
