5f2a23ec9ca9dd580601424d2a1a0a62398d8271
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-19 12:59:37 +0545

    01 finished

--- lib/pages/json_01.dart
+++ lib/pages/json_01.dart
@@ -10,15 +10,48 @@ class JSON01 extends StatefulWidget {
 }
 
 class _JSON01State extends State<JSON01> {
+  //fields
+  var url;
+  var response;
+  var data;
+  bool isLoading = true;
+
+  //Future
+  Future fetchData() async {
+    url = 'https://jsonplaceholder.typicode.com/posts';
+    response = await http.get(url);
+    setState(() {
+      data = convert.jsonDecode(response.body);
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('JSON Serialization'),
       ),
-      body: Center(
-        child: Text('JSON '),
-      ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(data[index]['title']),
+                  subtitle: Text(data[index]['id']),
+                );
+              },
+            ),
     );
   }
 }
