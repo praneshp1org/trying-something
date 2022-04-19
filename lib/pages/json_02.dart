5fc5c9547949f1fbdd23df53158f2ce0b5b16eb7
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-19 21:28:17 +0545

    json to preferrred dart objects completed sort of

--- lib/pages/json_02.dart
+++ lib/pages/json_02.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/models/comments_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
@@ -10,15 +11,52 @@ class JSON02 extends StatefulWidget {
 }
 
 class _JSON02State extends State<JSON02> {
+  var url, response;
+  late List data;
+  late List<Comment> comment;
+  bool isLoading = true;
+
+  Future getComment() async {
+    url = Uri.parse('https://jsonplaceholder.typicode.com/comments');
+    response = await http.get(url);
+    data = convert.jsonDecode(response.body);
+    setState(() {
+      comment = data.map((json) => Comment.fromJson(json)).toList();
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getComment();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('JSON Serialization 02'),
       ),
-      body: Center(
-        child: Text('JSON here'),
-      ),
+      body: (isLoading == true)
+          ? LinearProgressIndicator()
+          : ListView.builder(
+              itemCount: comment.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      comment[index].name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(comment[index].email),
+                    Text(comment[index].body),
+                  ],
+                );
+              },
+            ),
     );
   }
 }
