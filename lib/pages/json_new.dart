349eb95e74d872f6ff47f4ad4733a2c5ac11d9cb
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-20 20:57:23 +0545

    this completed

--- lib/pages/json_new.dart
+++ lib/pages/json_new.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/models/c_model.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert' as convert;
 
@@ -11,8 +12,59 @@ class JSONNew extends StatefulWidget {
 }
 
 class _JSONNewState extends State<JSONNew> {
+  var url, response;
+  late List data;
+  bool isLoading = true;
+  late List<Comment2> comment2;
+  Future getComments() async {
+    url = Uri.parse("https://jsonplaceholder.typicode.com/comments");
+
+    response = await http.get(url);
+    data = convert.jsonDecode(response.body);
+    setState(() {
+      comment2 = data.map((json2) => Comment2.fromJSON(json2)).toList();
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getComments();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Dynamic JSON to preferred Dart Objects'),
+        ),
+        body: (isLoading == true)
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 10,
+                    child: Container(
+                      //height: 150,
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ListTile(
+                            title: Text(comment2[index].name),
+                            subtitle: Text(comment2[index].email),
+                          ),
+                          Text(comment2[index].body)
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ));
   }
 }
