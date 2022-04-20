18dbfeb5d0968fe24d650f7c203ff70c4524548a
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-20 19:31:21 +0545

    new model

---
+++ lib/pages/json_new.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+
+import 'package:http/http.dart' as http;
+
+class JSONNew extends StatefulWidget {
+  const JSONNew({Key? key}) : super(key: key);
+
+  @override
+  State<JSONNew> createState() => _JSONNewState();
+}
+
+class _JSONNewState extends State<JSONNew> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
